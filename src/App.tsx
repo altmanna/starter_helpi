4ba11449c465f7320fb4611035e10b73e91e9ef8
Andrew Altmann <altmanna@udel.edu>
2025-03-18 23:09:39 -0400

    trying names again

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 
-//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
+// local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
 const saveKeyData = "MYKEY";
 const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
